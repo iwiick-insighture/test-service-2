c0f99de1880e08d8b196ffba45a317300b321f7e
iwiick <135204780+iwiick-insighture@users.noreply.github.com>
2024-02-07 14:57:54 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,19 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+import { Handler } from 'aws-lambda';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { createServer, proxy } from 'aws-serverless-express';
+
 import { description, name, version } from '../package.json';
 
-
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useLogger(process.env.NODE_ENV !== 'production' ? ['debug'] : ['warn']);
-  app.useGlobalPipes(new ValidationPipe());
-  app.setGlobalPrefix('api/v1/pipeline');
+  const expressApp = require('express')();
+  const nestApp = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+  
+  nestApp.useLogger(process.env.NODE_ENV !== 'production' ? ['debug'] : ['warn']);
+  nestApp.useGlobalPipes(new ValidationPipe());
+  nestApp.setGlobalPrefix('api/v1/pipeline');
 
   const config = new DocumentBuilder()
     .setTitle(name)
@@ -19,14 +23,27 @@ async function bootstrap() {
     .setVersion(version)
     .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document, {
+  const document = SwaggerModule.createDocument(nestApp, config);
+  SwaggerModule.setup('docs', nestApp, document, {
     swaggerOptions: {
       tagsSorter: 'alpha',
       operationsSorter: 'alpha',
     },
   });
-  app.enableCors();
-  await app.listen(4001);
+
+  nestApp.enableCors();
+
+  await nestApp.init();
+
+  const server = createServer(expressApp);
+
+  // Export your Nest.js application as a handler function
+  const lambdaHandler: Handler = (event, context) => {
+    return proxy(server, event, context);
+  };
+
+  return lambdaHandler;
 }
-bootstrap();
+
+// Export your AWS Lambda handler
+export const handler = bootstrap();
